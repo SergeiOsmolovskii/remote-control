6753684443ef2b31d734ab1ee896bce88eb96f8e
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-06-21 23:36:48 +0300

    feat: add mouse commands

--- index.ts
+++ index.ts
@@ -1,9 +1,35 @@
 import Jimp from 'jimp';
-import {httpServer} from './src/http_server/index.js';
+import { httpServer } from './src/http_server/index.js';
 import robot from 'robotjs';
 import { WebSocketServer } from 'ws';
+import { mouseCommadns } from './src/commands/mouseMoveCommands.js';
 
 const HTTP_PORT = 3000;
+const HTTP_WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({
+  port: HTTP_WS_PORT
+});
+
+wss.on('connection', (ws) => {
+  ws.on('message', function incoming(message) {
+    const command = message.toString().split(' ')[0];
+    const params = message.toString().split(' ').slice(1);
+    if (command === 'mouse_position' ) {
+      const curreentMousePosition = robot.getMousePos();
+      ws.send(`mouse_position ${curreentMousePosition.x},${curreentMousePosition.y}`);
+    } else if (command.includes('mouse_')) {
+      mouseCommadns(command, params);
+      ws.send(command);
+    } else if (command.includes('draw_')) {
+      console.log('draw');
+      ws.send(command);
+    } else if (command.includes('prnt_')) {
+      ws.send(command);
+      console.log('prnt');
+    }
+  });
+});
---
+++ src/commands/mouseMoveCommands.ts
@@ -0,0 +1,37 @@
+import robot from 'robotjs';
+import { WebSocketServer } from 'ws';
+
+export const mouseCommadns = (command: string, params: string[]) => {
+
+  switch (command) {
+    case 'mouse_up': mouseUp(params[0]);
+      break;
+    case 'mouse_down': mouseDown(params[0]);
+      break;
+    case 'mouse_left': mouseLeft(params[0]);
+      break;
+    case 'mouse_right': mouseRight(params[0]);
+      break;
+    default: return;
+  }
+};
+
+const mouseUp = (Y) => {
+  const curreentMousePosition = robot.getMousePos();
+  robot.moveMouse(curreentMousePosition.x, curreentMousePosition.y - Number(Y));
+}
+
+const mouseDown = (Y) => {
+  const curreentMousePosition = robot.getMousePos();
+  robot.moveMouse(curreentMousePosition.x, curreentMousePosition.y + Number(Y));
+}
+
+const mouseLeft = (X) => {
+  const curreentMousePosition = robot.getMousePos();
+  robot.moveMouse(curreentMousePosition.x - Number(X), curreentMousePosition.y);
+}
+
+const mouseRight = (X) => {
+  const curreentMousePosition = robot.getMousePos();
+  robot.moveMouse(curreentMousePosition.x + Number(X), curreentMousePosition.y);
+}
