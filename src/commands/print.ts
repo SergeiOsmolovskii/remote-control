67e220593b692932e869a33110fb76b707236b43
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-06-25 21:14:06 +0300

    fix: fix colors bug with 'print screen' command

--- src/commands/print.ts
+++ src/commands/print.ts
@@ -6,13 +6,22 @@ import { WebSocket } from 'ws';
 export const printScreen = (ws: WebSocket) => {
   const mousePos = robot.getMousePos();
   const currentX = mousePos.x - 100;
-  const currentY = mousePos.y - 100; 
-  const screen: string = robot.screen.capture(currentX, currentY, 200, 200).image;
+  const currentY = mousePos.y - 100;
+  const screen = robot.screen.capture(currentX, currentY, 200, 200).image;
 
-  new Jimp({data: screen, width: 200, height: 200}, (err: Error, image) => { 
+  new Jimp({ data: screen, width: 200, height: 200 }, (err: Error, image) => {
+    let pos = 0;
+
+    image.scan(0, 0, 200, 200, (x, y, idx) => {
+      image.bitmap.data[idx + 2] = screen.readUInt8(pos++);
+      image.bitmap.data[idx + 1] = screen.readUInt8(pos++);
+      image.bitmap.data[idx + 0] = screen.readUInt8(pos++);
+      image.bitmap.data[idx + 3] = screen.readUInt8(pos++);
+    });
+
     image.getBuffer(Jimp.MIME_PNG, (err: Error, buffer: string) => {
       const data = Buffer.from(buffer, 'base64');
       ws.send(`prnt_scrn ${data.toString('base64')}`);
-    }); 
+    });
   });
 };
